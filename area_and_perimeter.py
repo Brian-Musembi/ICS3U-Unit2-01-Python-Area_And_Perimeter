7cdb018d04b5facd81b9f0c95e07e3456502dc90
Brian Musembi <brian.musembi@mths.ca>
2021-04-26 14:40:16 +0000

    edited area_and_perimeter.py

--- area_and_perimeter.py
+++ area_and_perimeter.py
@@ -6,12 +6,13 @@
 
 import math
 
+
 def main():
     print("If a circle has a radius of:")
     print("15 mm")
     print("")
     print("The area of the circle is {} mm².".format(math.pi * 15 ** 2))
-    print("And the perimeter of the circle is {} mm.".format(2 *(math.pi * 15)))
+    print("The perimeter of the circle is {} mm.".format(2 * (math.pi * 15)))
     print("")
     print("Done")
 
